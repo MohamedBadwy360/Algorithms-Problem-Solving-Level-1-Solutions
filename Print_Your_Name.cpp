8ca905ed90a57595b98d49a23952ede38fec25cc
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-09-27 03:56:49 +0300

    Print your name

---
+++ Print_Your_Name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintName(string name)
+{
+    cout << "My name is " << name << endl;
+}
+
+int main()
+{
+    PrintName("Mohamed");
+    
+    return 0;
+}
