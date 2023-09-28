a2a7685eb26f61508e4d8a29241d2a20b6a5388d
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-09-28 11:52:23 +0300

    Sum Odd Numbers from 1 to N

---
+++ PrintSumOddNumbersFrom1toN.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int ReadNumber()
+{
+    int Number;
+    cout << "Enter a Number: ";
+    cin >> Number;
+
+    return Number;
+}
+
+void PrintSumOddNumberFromNto1(int Number)
+{
+    int Sum = 0;
+    for (int i = 1; i <= Number; i += 2)
+    {
+        Sum += i;
+    }
+
+    cout << Sum << endl;
+}
+
+
+int main()
+{
+    int Number = ReadNumber();
+    PrintSumOddNumberFromNto1(Number);
+
+    return 0; 
+}
+
+
